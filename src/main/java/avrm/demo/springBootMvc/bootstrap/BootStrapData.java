0f546d15fee13f5def1b1d4bf4b38a5d2e285dcc
avrmeduard <avrmeduard@gmail.com>
2020-07-14 09:53:52 +0300

    books added

--- src/main/java/avrm/demo/springBootMvc/bootstrap/BootStrapData.java
+++ src/main/java/avrm/demo/springBootMvc/bootstrap/BootStrapData.java
@@ -1,5 +1,7 @@
 package avrm.demo.springBootMvc.bootstrap;
 
+import avrm.demo.springBootMvc.domain.Author;
+import avrm.demo.springBootMvc.domain.Book;
 import avrm.demo.springBootMvc.repositories.AuthorRepository;
 import avrm.demo.springBootMvc.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +20,24 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Author ion = new Author("Ion", "Creanga");
+        Book capra = new Book("Capra cu 3 iezi", "42493077");
+
+        ion.getBooks().add(capra);
+        capra.getAuthors().add(ion);
+        authorRepository.save(ion);
+        bookRepository.save(capra);
+
+        Author mih = new Author("Mihai", "Eminescu");
+        Book poezii = new Book("Luceafarul", "99328290");
+
+        mih.getBooks().add(poezii);
+        poezii.getAuthors().add(mih);
+        authorRepository.save(mih);
+        bookRepository.save(poezii);
+
+        System.out.println("Bootstrap started \n Number of books added : " + bookRepository.count());
 
     }
 }
