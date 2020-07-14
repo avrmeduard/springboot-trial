9e8ac17d5a39c6054c526ab29e7a3c2f229ebfa4
avrmeduard <avrmeduard@gmail.com>
2020-07-14 08:20:57 +0300

    Jpa - manyToMany relation added

--- src/main/java/avrm/demo/springBootMvc/domain/Book.java
+++ src/main/java/avrm/demo/springBootMvc/domain/Book.java
@@ -14,7 +14,9 @@ public class Book {
     private String title;
     private String isbn;
 
-    @ManyToMany(mappedBy = "authors")
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set <Author> authors;
 
     public Book() {
