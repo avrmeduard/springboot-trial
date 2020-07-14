4c343822bd7b11ac5e7f25b587d307ffaab09495
avrmeduard <avrmeduard@gmail.com>
2020-07-15 02:32:51 +0300

    injected an instance of the bookRepository

--- src/main/java/avrm/demo/springBootMvc/controllers/BookControler.java
+++ src/main/java/avrm/demo/springBootMvc/controllers/BookControler.java
@@ -1,5 +1,6 @@
 package avrm.demo.springBootMvc.controllers;
 
+import avrm.demo.springBootMvc.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookControler {
 
+    private BookRepository bookRepository;
+
+    public BookControler(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @RequestMapping("/books")
     public String getBooks(Model model) {
 
+        model.addAttribute("books",bookRepository.findAll());
         return "books";
     }
 }
