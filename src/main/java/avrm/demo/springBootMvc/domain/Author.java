be3b0211187618aaf8917554539191f510d846bb
avrmeduard <avrmeduard@gmail.com>
2020-07-14 08:05:05 +0300

    Jpa - Entity added

--- src/main/java/avrm/demo/springBootMvc/domain/Author.java
+++ src/main/java/avrm/demo/springBootMvc/domain/Author.java
@@ -1,10 +1,17 @@
 package avrm.demo.springBootMvc.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String firstName;
