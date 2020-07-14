415431b16f79da9d87d1223723a1cedb3aef4729
avrmeduard <avrmeduard@gmail.com>
2020-07-14 09:10:58 +0300

    crud controler added

--- src/main/java/avrm/demo/springBootMvc/bootstrap/BootStrapData.java
+++ src/main/java/avrm/demo/springBootMvc/bootstrap/BootStrapData.java
@@ -1,4 +1,23 @@
 package avrm.demo.springBootMvc.bootstrap;
 
-public class BootStrapData {
+import avrm.demo.springBootMvc.repositories.AuthorRepository;
+import avrm.demo.springBootMvc.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository , BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
