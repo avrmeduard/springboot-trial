dc93f466e95d77a09ac1abdde60824097998a7e4
avrmeduard <avrmeduard@gmail.com>
2020-07-14 04:46:35 +0300

    First commit

---
+++ src/test/java/avrm/demo/springBootMvc/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package avrm.demo.springBootMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
