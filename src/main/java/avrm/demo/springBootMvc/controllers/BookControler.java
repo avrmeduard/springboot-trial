6e13a8e9237bdef009f99762c514a4493feeaa4b
avrmeduard <avrmeduard@gmail.com>
2020-07-15 02:15:14 +0300

    @controller added

--- src/main/java/avrm/demo/springBootMvc/controllers/BookControler.java
+++ src/main/java/avrm/demo/springBootMvc/controllers/BookControler.java
@@ -1,7 +1,12 @@
 package avrm.demo.springBootMvc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookControler {
 
+    @RequestMapping("/books")
     public String getBooks() {
 
         return "books";
