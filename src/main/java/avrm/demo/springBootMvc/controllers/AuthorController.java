4ae18de853806e4b8f3442f4011255d8037a4ef1
avrmeduard <avrmeduard@gmail.com>
2020-07-15 03:56:56 +0300

    controller package return edit

--- src/main/java/avrm/demo/springBootMvc/controllers/AuthorController.java
+++ src/main/java/avrm/demo/springBootMvc/controllers/AuthorController.java
@@ -1,8 +1,11 @@
 package avrm.demo.springBootMvc.controllers;
 
 import avrm.demo.springBootMvc.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class AuthorController {
 
     private AuthorRepository authorRepository;
@@ -11,9 +14,10 @@ public class AuthorController {
         this.authorRepository = authorRepository;
     }
 
+    @RequestMapping("/author")
     public String getAuthor(Model model) {
 
         model.addAttribute("author", authorRepository.findAll());
-        return "author";
+        return "author/list";
     }
 }
